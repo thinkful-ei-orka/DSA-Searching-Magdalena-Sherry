03a246e8a46e3adb99f7ef254320986e40db3312
Sherry McCarty <sherrydmccarty@gmail.com>
2020-07-22 11:21:25 -0400

    1

---
+++ searching-drills.js
@@ -0,0 +1,10 @@
+//1. How many searches?
+
+// 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 -- length: 10 / 2 => [5]
+// is 11 < or > or = 8?
+// 3, 5, 6, 8 -- length: 4 / 2 => [2]
+// is 5 < > = 8?
+// 6, 8 -- length 2 / 2 => [1]
+// is 6 < > = 8?
+// 8 === 8 ! We found it!!
+// 4 steps in total to find 8!
